6f7647b049e8b2bd9823a447db0bc798445bcb5c
Justas10k <115347112+Justas10k@users.noreply.github.com>
2023-09-29 17:54:52 +0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,9 @@
-let difficulty=30;
+let difficulty=2;
+let mistakes = 0;
+let timer_mistake_con= document.getElementById('timer_mistake_con')
 
-function getRandomIndices(length, count) {
-  const indices = Array.from({ length }, (_, i) => i);
-  for (let i = length - 1; i > length - count - 1; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [indices[i], indices[j]] = [indices[j], indices[i]];
-  }
-  return indices.slice(length - count);
-}
+
+let randomSudokuBoard = generateRandomSudokuBoard();
 
 function generateRandomSudokuBoard() {
   const board = [];
@@ -20,14 +16,14 @@ function generateRandomSudokuBoard() {
   }
 
   function isValidMove(row, col, num) {
-    // Check if the number is not already in the row or column
+    // Check if not in row or column
     for (let i = 0; i < 9; i++) {
       if (board[row][i] === num || board[i][col] === num) {
         return false;
       }
     }
 
-    // Check if the number is not in the 3x3 subgrid
+    // Check if not in 3x3 
     const startRow = Math.floor(row / 3) * 3;
     const startCol = Math.floor(col / 3) * 3;
     for (let i = startRow; i < startRow + 3; i++) {
@@ -80,6 +76,8 @@ function generateRandomSudokuBoard() {
   return board;
 }
 
+const partialSudokuBoard = generatePartialSudokuBoard(randomSudokuBoard, difficulty);
+
 function generatePartialSudokuBoard(board, numToRemove) {
   const partialBoard = board.map(row => [...row]);
 
@@ -93,18 +91,88 @@ function generatePartialSudokuBoard(board, numToRemove) {
   return partialBoard;
 }
 
+function getRandomIndices(length, count) {
+  const indices = Array.from({ length }, (_, i) => i);
+  for (let i = length - 1; i > length - count - 1; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [indices[i], indices[j]] = [indices[j], indices[i]];
+  }
+  return indices.slice(length - count);
+}
+
+displaySudokuBoard(partialSudokuBoard, 'sudoku-board');
+function displaySudokuBoard(board, containerId) {
+  const sudokuBoard = document.getElementById(containerId);
+  sudokuBoard.innerHTML = ''; // Clear previous content
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.innerText = board[i][j] !== 0 ? board[i][j] : '';
+
+      if (board[i][j] === 0) {
+        cell.classList.add('empty-cell');
+        cell.dataset.row = i;
+        cell.dataset.col = j;
+        cell.addEventListener('dragover', allowDrop);
+        cell.addEventListener('drop', drop);
+      }
+
+      sudokuBoard.appendChild(cell);
+    }
+  }
+}
+///button start
+
+function resetBoardWithDifficulty(newDifficulty) {
+  difficulty = newDifficulty;
+
+  // Reset the timer
+  clearInterval(timerInterval);
+  timerRunning = false;
+  startTime = null;
+  const timerElement = document.getElementById('timer');
+  timerElement.innerText = 'Time: 00:00:00';
+  startTimer();
+  mistakes = 0;
+  updateMistakesCounter();
+
+  generateNewRandomSudokuBoard();
+  const partialSudokuBoard = generatePartialSudokuBoard(randomSudokuBoard, difficulty);
+  displaySudokuBoard(partialSudokuBoard, 'sudoku-board');
+//drag drop
+  const emptyCells = document.querySelectorAll('.empty-cell');
+  emptyCells.forEach(emptyCell => {
+    emptyCell.addEventListener('dragover', allowDrop);
+    emptyCell.addEventListener('drop', drop);
+  });
+}
+
+function generateNewRandomSudokuBoard() {
+  randomSudokuBoard = generateRandomSudokuBoard();
+}
+
+const difficultyButtons = document.querySelectorAll('.difficulty-button');
+difficultyButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    const newDifficulty = parseInt(event.target.dataset.difficulty);
+    resetBoardWithDifficulty(newDifficulty);
+  });
+});
+
+const resetButton = document.getElementById('play-again');
+resetButton.addEventListener('click', () => {
+  resetBoardWithDifficulty(difficulty);
+});
+//button end//
+
+
+//drag
 document.addEventListener('dragstart', () => {
   startTimer();
 });
 
-function updateMistakesCounter() {
-  const mistakesCounter = document.getElementById('mistakes-counter');
-  mistakesCounter.innerText = `Mistakes: ${mistakes}`;
-}
-
-function checkSudokuValidity(row, col, num) {
-  return randomSudokuBoard[row][col] === num;
-}
 function allowDrop(event) {
   event.preventDefault();
 }
@@ -112,32 +180,6 @@ function allowDrop(event) {
 function drag(event) {
   event.dataTransfer.setData("text", event.target.innerText);
 }
-
-let mistakes = 0;
-
-const randomSudokuBoard = generateRandomSudokuBoard();
-const partialSudokuBoard = generatePartialSudokuBoard(randomSudokuBoard, difficulty);
-
-function displaySudokuBoard(board, containerId) {
-  const sudokuBoard = document.getElementById(containerId);
-
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      if (board[i][j] === 0) {
-        cell.classList.add('empty-cell');
-        cell.dataset.row = i;
-        cell.dataset.col = j;
-      }
-      cell.innerText = board[i][j] !== 0 ? board[i][j] : '';
-      sudokuBoard.appendChild(cell);
-    }
-  }
-}
-
-displaySudokuBoard(partialSudokuBoard, 'sudoku-board');
-
 const draggableNumbers = document.querySelectorAll('.draggable');
 const emptyCells = document.querySelectorAll('.empty-cell');
 
@@ -150,15 +192,20 @@ emptyCells.forEach(emptyCell => {
   emptyCell.addEventListener('drop', drop);
 });
 
-// Display mistakes counter
+
+function updateMistakesCounter() {
+  const mistakesCounter = document.getElementById('mistakes-counter');
+  mistakesCounter.innerText = `Mistakes: ${mistakes}`;
+}
+
 const mistakesContainer = document.createElement('div');
 mistakesContainer.id = 'mistakes-container';
 mistakesContainer.innerHTML = '<h2 id="mistakes-counter">Mistakes: 0</h2>';
-document.body.appendChild(mistakesContainer);
+timer_mistake_con.appendChild(mistakesContainer);
 updateMistakesCounter();
 
 
-
+//timer
 let startTime;
 let endTime;
 let timerInterval;
@@ -168,22 +215,28 @@ let timerRunning = false;
 function startTimer() {
   if (!timerRunning) {
     startTime = Date.now();
-    timerInterval = setInterval(updateTimer, 1000);
     timerRunning = true;
-  }function updateTimer() {
-    const currentTime = Date.now();
-    const elapsedTime = currentTime - startTime;
-  
-    // Calculate hours, minutes, and seconds
-    const hours = Math.floor(elapsedTime / 3600000); 
-    const remainingTime = elapsedTime % 3600000; 
-    const minutes = Math.floor(remainingTime / 60000);  
-    const seconds = Math.floor((remainingTime % 60000) / 1000); 
-  
-    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    document.getElementById('timer').innerText = `Time: ${formattedTime}`;
+
+    timerInterval = setInterval(() => {
+      updateTimer();
+    }, 1000);
   }
 }
+
+function updateTimer() {
+  const currentTime = Date.now();
+  const elapsedTime = currentTime - startTime;
+
+  // Calculate hours, minutes, and seconds
+  const hours = Math.floor(elapsedTime / 3600000);
+  const remainingTime = elapsedTime % 3600000;
+  const minutes = Math.floor(remainingTime / 60000);
+  const seconds = Math.floor((remainingTime % 60000) / 1000);
+
+  const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  document.getElementById('timer').innerText = `Time: ${formattedTime}`;
+}
+
 
 function stopTimer() {
   clearInterval(timerInterval);
@@ -203,9 +256,36 @@ function stopTimer() {
   });
 
   if (completed) {
-    alert("good job");
+    const currentTime = Date.now();
+    const elapsedTime = (currentTime - startTime) / 1000;
+
+    const difficultyNames = {
+      10: 'Super Easy',
+      20: 'Easy',
+      35: 'Medium',
+      50: 'Hard'
+    };
+
+    const modalTime = document.getElementById('modal-time');
+    modalTime.innerText = `Time taken: ${Math.floor(elapsedTime / 3600)} hours, ${Math.floor((elapsedTime % 3600) / 60)} minutes, ${Math.floor(elapsedTime % 60)} seconds`;
+
+    const modalMistakes = document.getElementById('modal-mistakes');
+    modalMistakes.innerText = `Mistakes: ${mistakes}`;
+
+    const modalDifficulty = document.getElementById('modal-difficulty');
+    modalDifficulty.innerText = `Difficulty: ${difficultyNames[difficulty]}`;
+
+    // Display the modal
+    const modal = document.getElementById('modal');
+    modal.style.display = 'block';
   }
 }
+
+function closeModal() {
+  const modal = document.getElementById('modal');
+  modal.style.display = 'none';
+}
+
 
 function drop(event) {
   event.preventDefault();
@@ -243,20 +323,10 @@ function drop(event) {
     }
   }
 }
-
+function checkSudokuValidity(row, col, num) {
+  return randomSudokuBoard[row][col] === num;
+}
 const timerContainer = document.createElement('div');
 timerContainer.id = 'timer-container';
 timerContainer.innerHTML = '<h2 id="timer">Time: 00:00:00</h2>';
-document.body.appendChild(timerContainer);
-
-
-
-
-
-// console.log('Random Sudoku Board:');
-// console.log(randomSudokuBoard.map(row => row.join(' ')).join('\n'));
-
-// console.log('\nPartial Sudoku Board (20 numbers removed):');
-// console.log(partialSudokuBoard.map(row => row.join(' ')).join('\n'));
-
-
+timer_mistake_con.appendChild(timerContainer);
